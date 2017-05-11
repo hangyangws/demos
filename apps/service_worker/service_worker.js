745d52cb93df9fa787a9779327347c16a049e7ca
hangyangws <hangyangws@qq.com>
2017-05-12 03:40:08 +0800

    sw

--- apps/service_worker/service_worker.js
+++ apps/service_worker/service_worker.js
@@ -1,59 +1,74 @@
-let CACHE_NAME = 'my-site-cache-v1',
+const CACHE_NAME = 'cache-1',
   urlsToCache = [
     '/',
-    '/styles/main.css',
-    '/script/main.js'
+    '/ico.ico',
+    '/index.html',
+    'static/main.js',
+    'static/index.js',
+    'static/index.css',
+    'static/img/img1.JPG',
+    'static/img/img2.jpg',
+    'static/img/img3.jpg',
+    'static/img/img4.jpg',
+    'static/img/img5.jpg',
+    'static/img/img6.jpg',
+    'static/img/img7.jpg',
+    'static/img/img8.jpg',
+    'static/img/img9.jpg',
+    'static/img/img10.jpg',
+    'static/img/img11.jpg'
   ]
 
-// 安装回调
-self.addEventListener('install', function(event) {
-  // 打开缓存
-  // 缓存文件
-  // 确认所有需要的资产是否缓存
+// 第一个响应事件：安装
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
+    .open(CACHE_NAME) // 打开某个“缓存库”
+    .then((cache) => {
       console.log(`缓存 ${CACHE_NAME} 已经开启`)
-      return cache.addAll(urlsToCache)
+      return cache.addAll(urlsToCache) // 向“缓存库”添加缓存文件
     })
   )
 })
 
-// 监听当前目录权限下的 fetch 事件
-// 如果有缓存则使用缓存，没有缓存则发出请求
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches
-//     .match(event.request)
-//     .then(function(response) {
-//       if (response) {
-//         return response
-//       }
-//       return fetch(event.request)
-//     })
-//   )
-// })
+// 第二个响应事件：激活
+// 一般在 activate 回调中进行缓存管理，原因在于：
+// 如果在安装步骤中清除了任何旧缓存，那么控制所有当前页面的任何旧服务工作线程将突然无法从缓存中提供文件
+self.addEventListener('activate', (event) => {
 
-// 监听当前目录权限下的 fetch 事件
-// 如果有缓存则使用缓存，没有缓存则发出请求
-// 获得请求 response 后，确保响应有效
-// 检查并确保响应的状态为 200
-// 确保响应类型为 basic，亦即由自身发起的请求。 这意味着，对第三方资产的请求不会添加到缓存（跨域不缓存）
+  var cacheWhitelist = ['cache-1'] // “缓存库”白名单
+
+  event.waitUntil(
+    caches.keys()
+    .then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
+// 监听作用域下的 fetch 事件
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches
-    .match(event.request)
+    caches.match(event.request) // 优先读取缓存
+    .catch(function() { // 没有缓存，直接访问线上数据
+      return fetch(event.request)
+    })
     .then(function(response) {
-      if (response) {
+      if (response) { // 有缓存，且缓存内容不为空，则使用缓存
         return response
       }
 
-      var fetchRequest = event.request.clone()
-
-      return fetch(fetchRequest).then(
-        function(response) {
-          // Check if we received a valid response
+      // 缓存内容为空，继续发送请求
+      return fetch(event.request)
+        .then(function(response) {
+          // 检查并确保响应的状态为 200，确保响应类型不是跨域
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response
           }
@@ -67,30 +82,10 @@ self.addEventListener('fetch', function(event) {
             })
 
           return response
-        }
-      )
-    })
-  )
-})
-
-// 一般在 activate 回调中进行缓存管理，原因在于：
-// 如果在安装步骤中清除了任何旧缓存，那么控制所有当前页面的任何旧服务工作线程将突然无法从缓存中提供文件
-
-// 监听激活事件
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1']
-
-  event.waitUntil(
-    // caches.keys() 返回promise对象，数据为旧版的 catch 名称数组
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName)
-          }
         })
-      )
+    })
+    .catch(function() { // 线上数据访问不到：断网，或者路径出错
+      return caches.match('/static/img/error.jpg')
     })
   )
 })
